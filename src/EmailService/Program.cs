30f7aa362e69c8746ddeb032a8eaf071133dd4f1
Nikita <34120307+ColdDoshirak@users.noreply.github.com>
2025-12-10 17:30:46 +0500

    dbcontext

--- src/EmailService/Program.cs
+++ src/EmailService/Program.cs
@@ -1,4 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using NotificationService.Models.DataModels;
+
 var builder = Microsoft.Extensions.Hosting.Host.CreateApplicationBuilder(args);
+
+// Add Entity Framework
+builder.Services.AddDbContext<NotificationContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add custom services
 builder.Services.AddScoped<global::EmailService.Services.EmailNotificationService>();
