b3904bd9a2914d8dd6eaf56c5f5f4f88d4ea0479
Nikita <34120307+ColdDoshirak@users.noreply.github.com>
2025-12-10 17:31:41 +0500

    dbcontext

--- src/PushService/Program.cs
+++ src/PushService/Program.cs
@@ -1,7 +1,13 @@
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using NotificationService.Models.DataModels;
 using PushService.Workers;
 
 var builder = Host.CreateApplicationBuilder(args);
+
+// Add Entity Framework
+builder.Services.AddDbContext<NotificationContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add custom services
 builder.Services.AddScoped<PushService.Services.PushService>();
